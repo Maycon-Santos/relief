97b1316408f137c1660457d158e171e876697ebd
Maycon Santos <mayconsantos.dev@gmail.com>
2026-02-18 23:21:42 -0300

    feat: add application icons for macOS, Windows, and Linux

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	application := app.NewApp()
@@ -28,6 +33,20 @@ func main() {
 		OnShutdown:       application.Shutdown,
 		Bind: []interface{}{
 			application,
+		},
+		Mac: &mac.Options{
+			TitleBar:             mac.TitleBarDefault(),
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "Relief Orchestrator",
+				Message: "© 2026 Omelete. Ferramenta de orquestração de desenvolvimento local.",
+				Icon:    icon,
+			},
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
 		},
 	})
 
